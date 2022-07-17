fee869a011a32d68e627f670252376eb4216d2dc
Jamerson Souza <jamersonsouza@outlook.com>
2022-07-17 15:35:30 -0300

    Api de Livros

---
+++ src/main/java/com/james/bookstore/Entidades/Livro.java
@@ -0,0 +1,15 @@
+package com.james.bookstore.Entidades;
+
+public class Livro {
+    
+    private Long id;
+    private String titulo;
+    private String nomeAutor;
+    private String texto;
+
+
+    private Categoria categoria;
+
+    
+
+}
