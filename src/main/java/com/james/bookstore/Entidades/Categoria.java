77ff145c73de57cf192f228bbd765f6ba84704fb
Jamerson Souza <jamersonsouza@outlook.com>
2022-07-23 09:15:29 -0300

    anotando e relacionando classes de dominio

--- src/main/java/com/james/bookstore/Entidades/Categoria.java
+++ src/main/java/com/james/bookstore/Entidades/Categoria.java
@@ -1,15 +1,32 @@
 package com.james.bookstore.Entidades;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Categoria {
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria implements Serializable{
     
-    private Long id;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
     private String nome;
     private String descricao;
 
     //relacionamento 1 categoria pode ter varios livros
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     
--- src/main/java/com/james/bookstore/Entidades/Livro.java
+++ src/main/java/com/james/bookstore/Entidades/Livro.java
@@ -1,15 +1,31 @@
 package com.james.bookstore.Entidades;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Livro implements Serializable{
     
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String nomeAutor;
     private String texto;
 
-
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
 
